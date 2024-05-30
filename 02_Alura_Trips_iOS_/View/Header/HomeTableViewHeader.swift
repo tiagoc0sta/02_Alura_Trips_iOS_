e76bde93c977bbfab2a9ea0c7f02e9876ea1e652
tiagoc0sta <tiagodpv@gmail.com>
2024-05-30 13:01:29 -0400

    add radious

--- 02_Alura_Trips_iOS_/View/Header/HomeTableViewHeader.swift
+++ 02_Alura_Trips_iOS_/View/Header/HomeTableViewHeader.swift
@@ -17,8 +17,19 @@ class HomeTableViewHeader: UIView {
     
     @IBOutlet weak var bannerImageView: UIImageView!
     
+    
+    @IBOutlet weak var bannerView: UIView!
+    
     func configureView(){
         headerView.backgroundColor = UIColor(red: 30/255, green: 59/255, blue: 119/255, alpha: 1)
+        
+        bannerView.layer.cornerRadius = 10
+        bannerView.layer.masksToBounds = true
+        
+        headerView.layer.cornerRadius = 500
+        headerView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        
     }
+   
     
 }
